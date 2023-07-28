4bfd54bbc69fdc2a64adc71e2b885b54631565b4
dimpram101 <dimas2004211@gmail.com>
2023-07-28 19:23:06 +0800

    prepare

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className='text-7xl font-bold'>test</p>
+    </>
+  )
+}
+
+export default App
