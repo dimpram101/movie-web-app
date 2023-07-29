0da0601d4d5b68e94c6ffd618e1e68b4cb16bcaf
dimpram101 <dimas2004211@gmail.com>
2023-07-29 16:50:58 +0800

    fix link redirect

--- src/components/ContentCard.jsx
+++ src/components/ContentCard.jsx
@@ -3,11 +3,11 @@ import moment from "moment";
 import LinkButton from "./LinkButton";
 
 const ContentCard = (props) => {
-  const { data } = props;
+  const { data, linkTo } = props;
   return (
     <div className="relative group overflow-hidden">
       <img
-        src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
+        src={`https://image.tmdb.org/t/p/w500/${data.poster_path ?? data.profile_path}`}
         alt=""
         className="rounded-lg z-0"
       />
@@ -15,10 +15,10 @@ const ContentCard = (props) => {
         <div className="flex flex-col justify-between h-full translate-y-[430px] group-hover:translate-y-0 transition-all duration-300 ease-in-out">
           <h1 className="text-2xl font-sofia">{`${
             data.title ?? data.name ?? data.original_name
-          } (${moment(data.release_date).format("YYYY")})`}</h1>
-          <div className="flex flex-col gap-2">
-            <p>{data.overview}</p>
-            <LinkButton to="/" label="Detail"/>
+          } ${data.poster_path ? `(${moment(data.release_date).format("YYYY")})` : ''}`}</h1>
+          <div className="flex flex-col">
+            <p className="h-72 w-full overflow-ellipsis truncate whitespace-normal flex flex-col">{data.overview}</p>
+            <LinkButton to={`/${linkTo}/`} label="Detail" className="flex-grow"/>
           </div>
         </div>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,8 @@ import LinkButton from "../components/LinkButton";
 
 const Home = () => {
   const [movieData, setMovieData] = useState([]);
+  const [tvData, setTvData] = useState([]);
+  const [actorData, setActorData] = useState([]);
   const [error, setError] = useState(null);
 
   useEffect(() => {
@@ -12,19 +14,57 @@ const Home = () => {
       .get("/discover/movie")
       .then((res) => setMovieData(res.data.results))
       .catch((err) => setError(err));
+    api
+      .get("/discover/tv?with_original_language=en")
+      .then((res) => setTvData(res.data.results))
+      .catch((err) => setError(err));
+    api
+      .get("/person/popular")
+      .then((res) => setActorData(res.data.results))
+      .catch((err) => setError(err));
   }, []);
 
+  console.log(actorData);
+
   return (
-    <div>
+    <div className="pb-12">
       {error && <>{error}</>}
       <div className="w-full">
         <div className="flex flex-row justify-between items-center mb-3">
-          <h1 className="text-2xl font-sofia">Discover Movie</h1>
-          <LinkButton className="w-24" to="/" label="See More"/>
+          <h1 className="text-2xl font-sofia">Discover TV Series</h1>
+          <LinkButton className="w-24" to="/" label="See More" />
         </div>
         <div className="grid grid-cols-5 grid-rows-4 gap-2">
           {movieData.length > 0 ? (
-            movieData.map((data) => <ContentCard data={data} key={data.id} />)
+            movieData.map((data) => <ContentCard data={data} linkTo="movies" key={data.id} />)
+          ) : (
+            <>WAIT</>
+          )}
+        </div>
+      </div>
+
+      <div className="w-full mt-12">
+        <div className="flex flex-row justify-between items-center mb-3">
+          <h1 className="text-2xl font-sofia">Popular Actor</h1>
+          <LinkButton className="w-24" to="/" label="See More" />
+        </div>
+        <div className="grid grid-cols-5 grid-rows-4 gap-2">
+          {tvData.length > 0 ? (
+            tvData.map((data) => <ContentCard data={data} linkTo="tv" key={data.id} />)
+          ) : (
+            <>WAIT</>
+          )}
+        </div>
+      </div>
+
+      <div className="w-full mt-12">
+        <div className="flex flex-row justify-between items-center mb-3">
+          <h1 className="text-2xl font-sofia">Popular Actor</h1>
+          <LinkButton className="w-24" to="/" label="See More" />
+        </div>
+        <div className="grid grid-cols-5 grid-rows-4 gap-2">
+          {actorData.length > 0 ? (
+            actorData.map((data) => <ContentCard data={data} linkTo="actors" key={data.id} />)
           ) : (
             <>WAIT</>
           )}
