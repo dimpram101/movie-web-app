46a84f8ee1eb7395eba96254b4bc54c8848682ba
dimpram101 <dimas2004211@gmail.com>
2023-07-30 16:00:19 +0800

    divide header component

---
+++ src/components/DetailHeadSection.jsx
@@ -0,0 +1,64 @@
+import moment from "moment/moment";
+import minutesToHm from "../utils/minutesToHours";
+import numberFormatter from "../utils/numberFormatter";
+
+/* eslint-disable react/prop-types */
+const DetailHeadSection = ({ data, rate }) => {
+  return (
+    <div className="flex flex-row gap-4">
+      <div className="w-[550px] h-[604px]">
+        <img
+          src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
+          className="h-full"
+        />
+      </div>
+      <div className="w-full flex flex-col justify-between font-sofia">
+        <div className="flex flex-col">
+          <h1 className="text-4xl font-extrabold">
+            {data.title} ({moment(data.release_date).format("YYYY")})
+          </h1>
+          <div className="flex items-center mb-3 text-lg">
+            <p>{rate}</p>
+            <p className="mx-2">|</p>
+            <p>{moment(data.release_date).format("MM/DD/YYYY")}</p>
+            <p className="mx-2">|</p>
+            <div className="flex gap-2">
+              {data.genres.map((genre) => (
+                <p key={genre.id}>{genre.name}</p>
+              ))}
+            </div>
+            <p className="mx-2">|</p>
+            <p>{minutesToHm(data.runtime)}</p>
+          </div>
+          <p className="text-xl italic font-thin mb-3">{data.tagline}</p>
+          <div className="flex flex-col text-lg">
+            <p className="font-bold text-orange-400">Overview</p>
+            <p className="">{data.overview}</p>
+          </div>
+        </div>
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-col">
+            <p className="text-orange-400 font-bold">Status</p>
+            <p className="font-normal">{data.status}</p>
+          </div>
+          <div className="flex flex-col">
+            <p className="text-orange-400 font-bold">Original Language</p>
+            <p className="font-normal">{data.spoken_languages[0].name}</p>
+          </div>
+          <div className="flex flex-col">
+            <p className="text-orange-400 font-bold">Budget</p>
+            <p className="font-normal">{numberFormatter.format(data.budget)}</p>
+          </div>
+          <div className="flex flex-col">
+            <p className="text-orange-400 font-bold">Revenue</p>
+            <p className="font-normal">
+              {numberFormatter.format(data.revenue)}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DetailHeadSection;
--- src/pages/movies/MovieDetail.jsx
+++ src/pages/movies/MovieDetail.jsx
@@ -3,12 +3,10 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import api from "../../api";
-import moment from "moment";
-import minutesToHm from "../../utils/minutesToHours";
 import Loading from "react-loading";
 import { useMemo } from "react";
-import numberFormatter from "../../utils/numberFormatter";
 import CastCard from "../../components/CastCard";
+import DetailHeadSection from "../../components/DetailHeadSection";
 
 const MovieDetail = () => {
   const { id } = useParams();
@@ -45,61 +43,7 @@ const MovieDetail = () => {
       )}
       {data && (
         <>
-          <div className="flex flex-row gap-4">
-            <div className="w-[550px] h-[604px]">
-              <img
-                src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
-                className="h-full"
-              />
-            </div>
-            <div className="w-full flex flex-col justify-between font-sofia">
-              <div className="flex flex-col">
-                <h1 className="text-4xl font-extrabold">
-                  {data.title} ({moment(data.release_date).format("YYYY")})
-                </h1>
-                <div className="flex items-center mb-3 text-lg">
-                  <p>{rate}</p>
-                  <p className="mx-2">|</p>
-                  <p>{moment(data.release_date).format("MM/DD/YYYY")}</p>
-                  <p className="mx-2">|</p>
-                  <div className="flex gap-2">
-                    {data.genres.map((genre) => (
-                      <p key={genre.id}>{genre.name}</p>
-                    ))}
-                  </div>
-                  <p className="mx-2">|</p>
-                  <p>{minutesToHm(data.runtime)}</p>
-                </div>
-                <p className="text-xl italic font-thin mb-3">{data.tagline}</p>
-                <div className="flex flex-col text-lg">
-                  <p className="font-bold text-orange-400">Overview</p>
-                  <p className="">{data.overview}</p>
-                </div>
-              </div>
-              <div className="flex flex-row justify-between">
-                <div className="flex flex-col">
-                  <p className="text-orange-400 font-bold">Status</p>
-                  <p className="font-normal">{data.status}</p>
-                </div>
-                <div className="flex flex-col">
-                  <p className="text-orange-400 font-bold">Original Language</p>
-                  <p className="font-normal">{data.spoken_languages[0].name}</p>
-                </div>
-                <div className="flex flex-col">
-                  <p className="text-orange-400 font-bold">Budget</p>
-                  <p className="font-normal">
-                    {numberFormatter.format(data.budget)}
-                  </p>
-                </div>
-                <div className="flex flex-col">
-                  <p className="text-orange-400 font-bold">Revenue</p>
-                  <p className="font-normal">
-                    {numberFormatter.format(data.revenue)}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+          <DetailHeadSection data={data} rate={rate}/>
           <div className="mt-12 font-sofia w-full">
             <h1 className="text-orange-400 text-3xl font-semibold">Casts</h1>
             <div className="mt-2 flex overflow-x-auto h-[26rem]">
