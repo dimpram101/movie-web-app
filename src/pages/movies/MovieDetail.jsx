beac7db6e0798a799b64b78a2fb1129850dc7210
dimpram101 <dimas2004211@gmail.com>
2023-07-30 15:43:31 +0800

    restyle moviedetail

--- src/pages/movies/MovieDetail.jsx
+++ src/pages/movies/MovieDetail.jsx
@@ -1,7 +1,7 @@
 // import React from 'react'
 
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import api from "../../api";
 import moment from "moment";
 import minutesToHm from "../../utils/minutesToHours";
@@ -27,7 +27,7 @@ const MovieDetail = () => {
       casts.push(data.credits.cast[i]);
     }
     return casts;
-  }, [data])
+  }, [data]);
 
   useEffect(() => {
     api
@@ -101,10 +101,30 @@ const MovieDetail = () => {
             </div>
           </div>
           <div className="mt-12 font-sofia w-full">
-            <h1 className="text-orange-400 text-3xl font-semibold">Cast</h1>
+            <h1 className="text-orange-400 text-3xl font-semibold">Casts</h1>
             <div className="mt-2 flex overflow-x-auto h-[26rem]">
               <div className="flex-shrink-0 flex space-x-4">
-                {casts.length > 0 && casts.map(cast => <CastCard cast={cast} key={cast}/>)}
+                {casts.length > 0 &&
+                  casts.map((cast) => <CastCard cast={cast} key={cast} />)}
+                <Link to={'/movies'} className="w-60 h-96 flex flex-col justify-center items-center gap-2 rounded-lg group hover:bg-opacity-50 hover:bg-orange-400 transition-colors duration-300 ease-in-out">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6 text-orange-400 group-hover:text-white transition-colors duration-300 ease-in-out"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                    />
+                  </svg>
+                  <p className="font-semibold text-orange-400 group-hover:text-white transition-colors duration-300 ease-in-out">
+                    See More Casts
+                  </p>
+                </Link>
               </div>
             </div>
           </div>
