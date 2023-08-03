d571a8a70142535db1cdf154ab82f70a1fc5f60d
dimpram101 <dimas2004211@gmail.com>
2023-08-03 22:59:17 +0800

    added comma in genre section

--- src/components/MovieDetailHeadSection.jsx
+++ src/components/MovieDetailHeadSection.jsx
@@ -1,7 +1,7 @@
 import moment from "moment/moment";
 import minutesToHm from "../utils/minutesToHours";
 import numberFormatter from "../utils/numberFormatter";
-import NoImage from '../assets/no-image.png'
+import NoImage from "../assets/no-image.png";
 
 /* eslint-disable react/prop-types */
 const MovieDetailHeadSection = ({ data, rate }) => {
@@ -9,7 +9,11 @@ const MovieDetailHeadSection = ({ data, rate }) => {
     <div className="flex flex-row gap-4">
       <div className="w-[550px] h-[604px]">
         <img
-          src={data.poster_path ? `https://image.tmdb.org/t/p/w500/${data.poster_path}` : NoImage}
+          src={
+            data.poster_path
+              ? `https://image.tmdb.org/t/p/w500/${data.poster_path}`
+              : NoImage
+          }
           className="h-full"
         />
       </div>
@@ -21,11 +25,18 @@ const MovieDetailHeadSection = ({ data, rate }) => {
           <div className="flex items-center mb-3 text-lg">
             <p>{rate}</p>
             <p className="mx-2">|</p>
-            <p>{data?.release_date ? moment(data.release_date).format("MM/DD/YYYY") : 'Unknown'}</p>
+            <p>
+              {data?.release_date
+                ? moment(data.release_date).format("MM/DD/YYYY")
+                : "Unknown"}
+            </p>
             <p className="mx-2">|</p>
             <div className="flex gap-2">
-              {data.genres.map((genre) => (
-                <p key={genre.id}>{genre.name}</p>
+              {data.genres.map((genre, index) => (
+                <p key={genre.id}>
+                  {genre.name}
+                  {index !== data.genres.length - 1 && ","}
+                </p>
               ))}
             </div>
             <p className="mx-2">|</p>
@@ -44,7 +55,9 @@ const MovieDetailHeadSection = ({ data, rate }) => {
           </div>
           <div className="flex flex-col">
             <p className="text-orange-400 font-bold">Original Language</p>
-            <p className="font-normal">{data.spoken_languages[0]?.name ?? 'Unknown'}</p>
+            <p className="font-normal">
+              {data.spoken_languages[0]?.name ?? "Unknown"}
+            </p>
           </div>
           <div className="flex flex-col">
             <p className="text-orange-400 font-bold">Budget</p>
